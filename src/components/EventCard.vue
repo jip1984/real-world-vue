5013006542022427a61f932fa677609b0648c05c
jon <jonprosser84@gmail.com>
2021-08-22 22:18:27 +0100

    vue mastery free weekend

---
+++ src/components/EventCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="event-card">
+    <!-- display event data -->
+    <span>@ {{ event.time }} on {{ event.date }}</span>
+    <h4>{{ event.title }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EventCard",
+  props: {
+    event: Object,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+.event-card:hover {
+  background-color: #333;
+  color: #fff;
+}
+</style>
